de19d171f6ddf8237e3a5a26d2552f0a0354464c
Jaka Katrasnik <jaka.katrasnik@sensum.eu>
2013-08-23 17:44:20 +0200

    new version

--- FastTexture/main.py
+++ FastTexture/main.py
@@ -4,15 +4,34 @@ import matplotlib.pyplot as plt
 
 
 def distance_calc(img):
-    tau = 2
+    tau = 5
+    sigma = 1.5
     xd, yd = img.shape
-    for i in xd:
-        for j in yd:
-            img[i - tau:i + tau + 1, j - tau:j + tau + 1]
-
+    img = img.astype('float64')
+    img = img/np.max(img)
+    result = np.zeros_like(img)
+    for i in range(xd):
+        for j in range(yd):
+            if i - tau >= 0 and i + tau < xd and j - tau >= 0 and j + tau < yd:
+                #import pdb;pdb.set_trace()
+                tmp = img[i - tau:i + tau + 1, j - tau:j + tau + 1]
+                gx, gy = np.gradient(tmp)
+                Mg = np.vstack((np.hstack((1 + gx**2, gx*gy)),
+                                np.hstack((gx*gy, 1 + gy**2))))
+                F = np.exp(-np.linalg.det(Mg)/sigma**2)
+                result[i, j] = F
+    return result
 
 if __name__ == '__main__':
     img = imread('grass&dirt.png')
-
+    result = distance_calc(img)
+    plt.subplot(121)
     plt.imshow(img, cmap='gray')
+    plt.subplot(122)
+    plt.imshow(result[10:-10, 10:-10])
+    plt.colorbar()
     plt.show()
+
+
+    #plt.imshow(img, cmap='gray')
+    #plt.show()
